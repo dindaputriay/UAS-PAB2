f1470cc4ef33b7c13199087a06bed90d6c22faab
dindaputriay <dindaputri@mhs.mdp.ac.id>
2023-06-11 12:52:42 +0700

    tampilan dan sedikit manifest

---
+++ app/src/main/java/com/example/uaspab/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.uaspab;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.uaspab.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+    private ActivitySplashBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding =ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getSupportActionBar().hide();
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },1000);
+
+
+    }
+}
